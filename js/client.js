08e8a7e879a087b8289242efe2f43cf789d522ee
D. Darren MacDonald <drrnmcdnld@gmail.com>
2014-12-18 20:31:24 -0400

    Added sample app and client for database

---
+++ js/client.js
@@ -0,0 +1,18 @@
+var pg = require('pg');
+
+var conString = "postgres://username:password@localhost/database";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
